8b2a9488bc733ff28ec4f6224d0f3b88d1bbebb6
Janet Walker <janetwalker@Janets-MBP.attlocal.net>
2017-03-12 01:53:25 -0500

    Added charactersand basic html and css layout.

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+// start with four characters
+// character The Doctor(HP 150), River Song(175), Dalek(200), Cyberman(210)
+//set each character as an object with the properties of name and hit points
+//user to select character to play
+//user chooses character and character shows in battle position
+//prompt user to select a battle opponent
+//user chooses opponent
+//user prompted to begin game by pressing the attach button
+//user presses the attack button
+//points decrease The Doctor(decrease oppontent by 20pts), River Song(decrease oppontent by 15pts), Dalek(decrease oppontent by 20pts), Cyberman(decrease oppontent by 15pts)
+
+var theDoctor {
+	name: "The" + " " + "Doctor";
+	hP: 150;
+}
+
+var riverSong {
+	name: "River" + " " + "Song";
+	hP: 175;
+}
+
+var dalek {
+	name: "Dalek"
+	hP: 200;
+}
+
+var cyberman {
+	name: "Cyberman";
+	hP: 210;
+}
+
+
+
+$(document).ready(function(){
+	
+
+});
+
+// 	var $thisElement = $("#theID");
+// 	console.log($thisElement);
+
+// 	var html = $thisElement.html();
+// 	$thisElement.text("coomes");
+
+
+
+// 	console.log("here");
+
+// var dog = {
+// 	name: "Ruby",
+// 	age: 2,
+// 	bark: function (ownersName) {
+// 		console.log("Bark " + ownersName);
+// 	},
+// 	vet: {firstName: "whatever"}
+
+// }
+//var thisElement = document.getElementById("id");
+
+//this is now an array
+// console.log(myPTagElements);
+
+
+// console.log(dog.vet.firstName);
+// console.log(dog.name);
+// console.log(dog.bark);
+
+// dog.bark("Janet")
+
+
